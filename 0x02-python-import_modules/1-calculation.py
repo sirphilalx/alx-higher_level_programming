47bbcc237d630e216f828c352873f28de8baed54
Philemon Okpokpa <remotephil.upwork@gmail.com>
2023-08-11 08:26:23 +0100

    as per task, the variables had to be on different lines, no destructuring

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
-a, b = 10, 5
+a = 10
+b = 5
 print(f"{a} + {b} = {add(a, b)}")
 print(f"{a} - {b} = {sub(a, b)}")
 print(f"{a} * {b} = {mul(a, b)}")
