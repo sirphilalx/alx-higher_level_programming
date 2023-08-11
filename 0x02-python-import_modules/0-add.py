89d6364cd624036299d88b0490c184a747774e3a
Philemon Okpokpa <remotephil.upwork@gmail.com>
2023-08-11 08:43:01 +0100

    changed the format from f-strint to %string

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-from add_0 import add as sum
-a = 1
-b = 2
-print("{:d} + {:d} = {:d}".format(a, b, sum(a, b)))
+if __name__ == "__main__":
+    from add_0 import add as sum
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, sum(a, b)))
