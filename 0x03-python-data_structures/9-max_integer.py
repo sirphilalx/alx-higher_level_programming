e0d1c4019f8197b2e260ff63df45318f4587dd26
Philemon Okpokpa <remotephil.upwork@gmail.com>
2023-08-14 16:11:58 +0100

    max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    highest = my_list[0]
+    for i in my_list:
+        if i > highest:
+            highest = i
+            return highest
+    return None
