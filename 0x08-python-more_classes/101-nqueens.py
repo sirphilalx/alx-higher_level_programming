c07466574f10327bd093c0458800d521e2363b57
Philemon Okpokpa <remotephil.upwork@gmail.com>
2023-09-09 02:36:13 +0100

    new files

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,81 +1,62 @@
 #!/usr/bin/python3
+"""
+nqueens backtracking program to print the coordinates of n queens
+on an nxn grid such that they are all in non-attacking positions
+"""
+
+
 from sys import argv
 
-if len(argv) is not 2:
-    print('Usage: nqueens N')
-    exit(1)
+if __name__ == "__main__":
+    a = []
+    if len(argv) != 2:
+        print("Usage: nqueens N")
+        exit(1)
+    if argv[1].isdigit() is False:
+        print("N must be a number")
+        exit(1)
+    n = int(argv[1])
+    if n < 4:
+        print("N must be at least 4")
+        exit(1)
 
-if not argv[1].isdigit():
-    print('N must be a number')
-    exit(1)
+    # initialize the answer list
+    for i in range(n):
+        a.append([i, None])
 
-N = int(argv[1])
+    def already_exists(y):
+        """check that a queen does not already exist in that y value"""
+        for x in range(n):
+            if y == a[x][1]:
+                return True
+        return False
 
-if N < 4:
-    print('N must be at least 4')
-    exit(1)
-
-
-def board_column_gen(board=[]):
-    if len(board):
-        for row in board:
-            row.append(0)
-    else:
-        for row in range(N):
-            board.append([0])
-    return board
-
-
-def add_queen(board, row, col):
-    board[row][col] = 1
-
-
-def new_queen_safe(board, row, col):
-    x = row
-    y = col
-
-    for i in range(1, N):
-        if (y - i) >= 0:
-            # check up-left diagonal
-            if (x - i) >= 0:
-                if board[x - i][y - i]:
-                    return False
-            # check left
-            if board[x][y - i]:
+    def reject(x, y):
+        """determines whether or not to reject the solution"""
+        if (already_exists(y)):
+            return False
+        i = 0
+        while(i < x):
+            if abs(a[i][1] - y) == abs(i - x):
                 return False
-            # check down-left diagonal
-            if (x + i) < N:
-                if board[x + i][y - i]:
-                    return False
-    return True
+            i += 1
+        return True
 
+    def clear_a(x):
+        """clears the answers from the point of failure on"""
+        for i in range(x, n):
+            a[i][1] = None
 
-def coordinate_format(candidates):
-    holberton = []
-    for x, attempt in enumerate(candidates):
-        holberton.append([])
-        for i, row in enumerate(attempt):
-            holberton[x].append([])
-            for j, col in enumerate(row):
-                if col:
-                    holberton[x][i].append(i)
-                    holberton[x][i].append(j)
-    return holberton
+    def nqueens(x):
+        """recursive backtracking function to find the solution"""
+        for y in range(n):
+            clear_a(x)
+            if reject(x, y):
+                a[x][1] = y
+                if (x == n - 1):  # accepts the solution
+                    print(a)
+                else:
+                    nqueens(x + 1)  # moves on to next x value to continue
 
-candidates = []
-candidates.append(board_column_gen())
-
-for col in range(N):
-    new_candidates = []
-    for matrix in candidates:
-        for row in range(N):
-            if new_queen_safe(matrix, row, col):
-                temp = [line[:] for line in matrix]
-                add_queen(temp, row, col)
-                if col < N - 1:
-                    board_column_gen(temp)
-                new_candidates.append(temp)
-    candidates = new_candidates
-
-for item in coordinate_format(candidates):
-    print(item)
+    # start the recursive process at x = 0
+    nqueens(0)
--- README.md
+++ README.md
@@ -6,3 +6,7 @@ Some of the directives inside this module include:
 ## 0x00-python-hello_world
 ## 0x01-python-if_else_loops_functions
 ## 0x02-python-import_modules
+## 0x03-python-data_structures
+## 0x04-python-more_data_structures
+## 0x05-python-exceptions
+## 0x06-python-classes
