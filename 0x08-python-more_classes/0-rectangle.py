441203d7a355690b597da5447320db93bf32d7c2
Philemon Okpokpa <remotephil.upwork@gmail.com>
2023-09-09 02:37:25 +0100

    new files

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Defines an empty class Rectangle
+"""
+Defines an empty class Rectangle
 """
 
 
 class Rectangle:
-    """Empty representation of a rectangle
-    """
+    """Empty representation of a rectangle"""
     pass
