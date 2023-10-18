c1409f86e4218ca115a6d93bd3d4e55a0654de8a
Philemon Okpokpa <remotephil.upwork@gmail.com>
2023-10-18 13:51:31 +0100

    creates a db and a table with several descriptors

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,6 @@
 -- a script that creates a db and a table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states(
     id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
     name VARCHAR(256) NOT NULL
