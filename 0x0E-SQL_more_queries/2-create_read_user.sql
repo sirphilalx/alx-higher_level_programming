170e75663e9eb3a2655bfb04532ad04d3d232ef1
Philemon Okpokpa <remotephil.upwork@gmail.com>
2023-10-18 13:20:20 +0100

    creates a db and a user and grants the select privilege

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,10 @@
+-- a script that creates a database and gives
+-- the select privilege to a user
+CREATE DATABASE
+IF NOT EXISTS hbtn_0d_2;
+CREATE USER
+IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
+GRANT SELECT
+ON hbtn_0d_2.*
+TO 'user_0d_2'@'localhost';
