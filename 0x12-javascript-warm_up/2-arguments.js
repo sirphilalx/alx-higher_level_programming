aca7b7736339add9435ad781ac3db4eb29f1bf48
Philemon Okpokpa <remotephil.upwork@gmail.com>
2023-11-14 00:49:26 +0100

    arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 
 /*
- * a process that outputs based on the number of arguments
+ * A script that prints a message depending of the number of arguments passed
  */
 
 const argCount = process.argv.length - 2;
 
-if (argCount === 2){
-    console.log('Arguments found');
-}else if(argCount === 1){
-    console.log('Argument found);
-}else{
-    console.log('No argument found');
+if (argCount === 2) {
+  console.log('Arguments found');
+} else if (argCount === 1) {
+  console.log('Argument found');
+} else {
+  console.log('No argument');
 }
