698fb9f7870a66f7bd450ac5e696cf5482622e73
Philemon Okpokpa <remotephil.upwork@gmail.com>
2023-11-14 00:57:53 +0100

    multilanguage

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+/*
+ * A script that prints 3 lines: (like 1-multi_languages.js)
+ * but by using an array of string and a loop
+ */
+
+const strArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < strArray.length; i++) {
+  console.log(strArray[i]);
+}
