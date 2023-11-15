dc5f979a989dcf94902b9d0a1b6f0e3aacb2e01c
Philemon Okpokpa <remotephil.upwork@gmail.com>
2023-11-15 23:06:56 +0100

    initialised class of Rectangle

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+/*
+ * initialised class of rectangle
+ */
+
+class Rectangle{
+    constructor(w, h){
+        this.width = w;
+        this.height = h;
+    }
+}
+
+module.exports = Rectangle;
