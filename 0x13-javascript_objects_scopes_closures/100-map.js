7835de74e388c2b3d2d4e903eceebbcb9fef2322
Philemon Okpokpa <remotephil.upwork@gmail.com>
2023-11-15 23:40:20 +0100

    map.js

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+/*
+ * A script that imports an array and computes a new array.
+ */
+
+const { list } = require('./100-data');
+
+const newList = list.map((value, index) => value * index);
+
+console.log(list);
+console.log(newList);
