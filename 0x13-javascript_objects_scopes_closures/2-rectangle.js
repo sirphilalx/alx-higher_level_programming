411745f502bca7bb10684dc649c4bba061eadf3f
Philemon Okpokpa <remotephil.upwork@gmail.com>
2023-11-15 23:15:09 +0100

    rectangle with a condition

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+/*
+ * initialised Rectangle class with a condition
+ */
+
+class Rectangle{
+  constructor (w, h) {
+    if (w > 0 && h > 0){
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
