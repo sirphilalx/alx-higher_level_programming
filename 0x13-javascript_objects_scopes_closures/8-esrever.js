84d379cd959d9762d5cb1012c595fe3eedcedf83
Philemon Okpokpa <remotephil.upwork@gmail.com>
2023-11-15 23:34:47 +0100

    reverse task

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+/*
+ * A function that returns the reversed version of a list:
+ */
+
+exports.esrever = function (list) {
+  const newList = [];
+
+  for (let i = list.length - 1; i >= 0; i--) {
+    newList.push(list[i]);
+  }
+  return newList;
+};
